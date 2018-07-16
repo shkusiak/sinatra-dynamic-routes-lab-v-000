51234b87a6fde312504d1c9692508013a4fe2e85
Shea Kusiak <shkusiak@gmail.com>
2018-07-16 03:06:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,14 +14,16 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     print_phrase = ""
+
     params[:number].to_i.times do
       print_phrase +=params[:phrase]
     end
+
     print_phrase
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
